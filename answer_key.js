3f49efcfaebcdea8e0168a7d88edd9cbfc368d5e
wanjoc <85894801+wanjoc@users.noreply.github.com>
2024-03-12 23:21:39 -0500

    removed instructions from answe key

--- answer_key.js
+++ answer_key.js
@@ -10,24 +10,6 @@ testButton.addEventListener("click", function () {
 
   console.log("Random Number: " + randomNumber);
 
-  // CODE ALONG
-  // - When the button is clicked, a random number between 0 and 99 is generated.
-  // - Depending on the value of this number, different actions will occur and a message will be displayed on the web page.:
-
-  // 1. If the number is less than 30
-  //  - The message should read: "The number is less than 30 🥶. You guessed {your input}
-  // 2. If the number is between 30 and 69,
-  //  - The message should read: "The number is between 30 and 69 😌. You guessed {your input}"
-
-  // 3. If the number is 70 or greater
-  //  - The message should read: "The number is 70 or greater 🥵. You guessed {your input}"
-
-  // Copy/paste these two lines of code below in each condition
-  // whatsYourGuess.style.display = "none";
-  // guessInput.style.display = "none";
-
-  // Extensions:
-  //  - Try modifying the conditions or the actions taken (such as changing the text or adding new conditions).
   let defaultGuessSentencePrefix = "You guessed ";
   if (randomNumber < 30) {
     result.innerHTML = "The temperature is less than 30" + "🥶.";
